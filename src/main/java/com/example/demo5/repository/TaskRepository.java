d7344ff5cc01b027a1ab8589da154dcfa7fb909c
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-10-20 22:15:07 +0200

    Utworzyłem interfejs TaskRepository

---
+++ src/main/java/com/example/demo5/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo5.repository;
+
+import com.example.demo5.model.TaskModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<TaskModel, Long> {
+}
