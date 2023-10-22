4d1e6dcb85b93cdd7c014b0e2c182054cb57eafb
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-10-22 22:08:59 +0200

    Zmiany w TaskService

--- src/main/java/com/example/demo5/service/TaskService.java
+++ src/main/java/com/example/demo5/service/TaskService.java
@@ -2,11 +2,13 @@ package com.example.demo5.service;
 
 import com.example.demo5.model.TaskModel;
 import com.example.demo5.repository.TaskRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class TaskService {
     private final TaskRepository taskRepository;
 
@@ -22,7 +24,24 @@ public class TaskService {
         taskRepository.save(task);
     }
 
-//    public void editTask(TaskModel task, Long id) {
-//        taskRepository.save(task, id);
-//    }
+    public TaskModel getTaskById(Long id)  {
+        return taskRepository.findById(id).orElse(null);
+    }
+
+    public void saveEditTask(TaskModel task, Long id) {
+        task.setId(id);
+        taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+    public List<TaskModel>findByColor(String name){
+        List<TaskModel> color = taskRepository.findByColor(name);
+    log.info("color {}", color);
+    return color;
+    }
+    public void updateDescription(Long id, String description){
+        taskRepository.updateDescriptionById(id, description);
+    }
 }
