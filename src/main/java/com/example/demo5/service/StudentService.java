73b0fba6fa3636987218c5637ef757b2c080b743
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-10-22 22:07:36 +0200

    Zmiany w StudentService

--- src/main/java/com/example/demo5/service/StudentService.java
+++ src/main/java/com/example/demo5/service/StudentService.java
@@ -1,12 +1,16 @@
 package com.example.demo5.service;
 
 import com.example.demo5.model.StudentModel;
+import com.example.demo5.model.TaskModel;
 import com.example.demo5.repository.StudentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 @Service
+@Slf4j
 public class StudentService {
     private final StudentRepository studentRepository;
 
@@ -17,7 +21,7 @@ public class StudentService {
     public void saveStudent(StudentModel student) {
     studentRepository.save(student);
     }
-    public List<StudentModel> getStudentsList(){
+    public List<StudentModel> getStudentList(){
         return studentRepository.findAll();
     }
 
@@ -32,5 +36,11 @@ public class StudentService {
 
     public void deleteStudent(Long id) {
         studentRepository.deleteById(id);
+    }
+    public List<StudentModel>sortByName(){
+        List<StudentModel> sortList = studentRepository.findSortedNamesOrderByLastNameAsc();
+        Stream<StudentModel> sortedList = sortList.stream().sorted();
+        log.info("last_name {}", sortedList);
+        return sortedList.toList();
     }
 }
