5b9ecc379a525cf3ab52e5483cd0061cb346b399
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-10-22 22:06:31 +0200

    Zmiany w student i task repozytory

--- src/main/java/com/example/demo5/repository/StudentRepository.java
+++ src/main/java/com/example/demo5/repository/StudentRepository.java
@@ -5,7 +5,10 @@ import org.hibernate.boot.jaxb.hbm.spi.JaxbHbmParentType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<StudentModel,Long> {
 
+    List<StudentModel> findSortedNamesOrderByLastNameAsc();
 }
