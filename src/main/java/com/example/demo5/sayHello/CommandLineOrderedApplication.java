2226a882d9a69792e7dee9a3e0806c97ea747a48
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-15 21:12:16 +0100

    Rozwiązanie zadania z SDA

---
+++ src/main/java/com/example/demo5/sayHello/CommandLineOrderedApplication.java
@@ -0,0 +1,11 @@
+package com.example.demo5.sayHello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandLineOrderedApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommandLineOrderedApplication.class, args);
+    }
+}
