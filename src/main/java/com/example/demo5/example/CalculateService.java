0d4a6f7c5a5abd4f564d3ca74520830b25633774
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-15 18:54:47 +0100

    Zmiana klasy serwisowej - dodanie adnotacji @Qualifier, która wstrzykuje odpowiedni bean

--- src/main/java/com/example/demo5/example/CalculateService.java
+++ src/main/java/com/example/demo5/example/CalculateService.java
@@ -1,17 +1,16 @@
 package com.example.demo5.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CalculateService {
     private final Shape shape;
-
-    @Autowired
-    public CalculateService(Shape shape) {
+@Autowired
+    public CalculateService(@Qualifier("rectangle") Shape shape) {
         this.shape = shape;
     }
-
     public void calculate() {
         shape.calculateArea();
     }
