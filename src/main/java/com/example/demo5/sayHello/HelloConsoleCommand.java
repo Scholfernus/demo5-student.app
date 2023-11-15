929c1a845c9fba463f3b0b31539fe62bacb09711
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-11-15 21:13:32 +0100

    Klasa HelloConsoleCommand

---
+++ src/main/java/com/example/demo5/sayHello/HelloConsoleCommand.java
@@ -0,0 +1,12 @@
+package com.example.demo5.sayHello;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloConsoleCommand implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.print("Hello\n");
+    }
+}
